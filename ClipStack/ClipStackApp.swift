cd4735dc745991ee583fb0cd2c5c34d62520c821
kyle-erikson <kyle.d.erikson@gmail.com>
2025-02-07 19:02:20 -0600

    Initial Commit

---
+++ ClipStack/ClipStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipStackApp.swift
+//  ClipStack
+//
+//  Created by Kyle Erikson on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipStackTests/ClipStackTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipStackTests.swift
+//  ClipStackTests
+//
+//  Created by Kyle Erikson on 2/7/25.
+//
+
+import Testing
+@testable import ClipStack
+
+struct ClipStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
